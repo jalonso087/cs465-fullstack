57bafc4b82c9ca7b90aead5235438e03d9ae3c19
Javier Alonso <sicotic87@gmail.com>
2026-08-09 11:21:28 -0400

    Module 5 completed baseline

---
+++ travlr/app_api/routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const tripsController = require("../controllers/trips");
+
+router.route("/trips").get(tripsController.tripsList); //GET method routes tripList
+
+//GET Method routes tripsFindByCode - requires parameter
+router
+    .route("/trips/:tripCode")
+    .get(tripsController.tripsFindByCode);
+
+module.exports = router;
